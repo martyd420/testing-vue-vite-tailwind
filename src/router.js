bc31a7e725350e3b57588b5c8a3b58629d812bc2
martyd420 <github-messages@pcdr.cz>
2022-01-16 19:17:36 +0100

    game over router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import StartScreen from './StartScreen.vue'
 import Questions from './Questions.vue'
+import GameOver from './pages/GameOver.vue'
 
 export default createRouter({
   history: createWebHistory(),
@@ -13,5 +14,9 @@ export default createRouter({
         path: '/questions',
         component: Questions,
     },
+    {
+      path: '/game-over',
+      component: GameOver,
+  },
   ]
 })
